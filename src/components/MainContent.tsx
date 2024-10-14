7bb89ee793b3aa65ecd9a59b29614de263de8c29
Adi4512 <adityasharma4510@gmail.com>
2024-10-14 17:09:44 +0530

    search checked removed

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useFilter } from "./FilterContext";
-import { SearchCheck, Tally3 } from "lucide-react";
+import { Tally3 } from "lucide-react";
 import axios from "axios";
 import BookCard from "./BookCard";
 const MainContent = () => {
